af4893f054acf1ee391fd35893933d6e22a01dc8
zwhile <zackwhile@outlook.com>
2019-07-17 15:22:18 -0400

    Updated setup.py for VIAME build.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
+
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
@@ -16,7 +17,12 @@ ext_modules = [
 ]
 
 setup(
-    name='toolkit',
-    packages=['toolkit'],
-    ext_modules=cythonize(ext_modules)
+    name='pysot',
+    package_data={'vot_iter': ['tracker_SiamRPNpp.m']},
+    packages=find_packages(exclude=('demo', 'experiments', 'testing_dataset',
+                                    'training_dataset')),
+    ext_modules=cythonize(ext_modules),
+    install_requires=['pyyaml', 'yacs', 'tqdm',
+                      'colorama', 'matplotlib',
+                      'cython', 'tensorboardX']
 )
