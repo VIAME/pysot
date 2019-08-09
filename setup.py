651e5f9125534a3500d8ce1b5d35988f8c2a3f0b
zwhile <zackwhile@outlook.com>
2019-08-09 14:53:56 -0400

    Initial push of conversion to single module.

--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ from Cython.Build import cythonize
 
 ext_modules = [
     Extension(
-        name='toolkit.utils.region',
+        name='pysot.toolkit.utils.region',
         sources=[
-            'toolkit/utils/region.pyx',
-            'toolkit/utils/src/region.c',
+            'pysot/toolkit/utils/region.pyx',
+            'pysot/toolkit/utils/src/region.c',
         ],
         include_dirs=[
-            'toolkit/utils/src'
+            'pysot/toolkit/utils/src'
         ]
     )
 ]
 
 setup(
     name='pysot',
-    package_data={'vot_iter': ['tracker_SiamRPNpp.m']},
-    packages=find_packages(exclude=('demo', 'experiments', 'testing_dataset',
-                                    'training_dataset')),
+    package_data={'pysot/vot_iter': ['tracker_SiamRPNpp.m']},
+    packages=find_packages(exclude=('pysot/demo', 'pysot/experiments', 'pysot/testing_dataset',
+                                    'pysot/training_dataset')),
     ext_modules=cythonize(ext_modules),
     install_requires=['pyyaml', 'yacs', 'tqdm',
                       'colorama', 'matplotlib',
