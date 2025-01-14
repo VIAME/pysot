5c089e4b510b75a08758952fdab7a9d3e48c9545
Matt Dawkins <matt.dawkins@kitware.com>
2025-01-13 22:22:28 -0500

    Update check

--- pysot/tracker/siammask_tracker.py
+++ pysot/tracker/siammask_tracker.py
@@ -37,7 +37,7 @@ class SiamMaskTracker(SiamRPNTracker):
     def _mask_post_processing(self, mask):
         target_mask = (mask > cfg.TRACK.MASK_THERSHOLD)
         target_mask = target_mask.astype(np.uint8)
-        if cv2.__version__[-5] == '4':
+        if cv2.__version__[0] == '4':
             contours, _ = cv2.findContours(target_mask,
                                            cv2.RETR_EXTERNAL,
                                            cv2.CHAIN_APPROX_NONE)
